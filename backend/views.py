691d08bc3e3eb0dfd94f9c0d88febd9665663998
Tevin Aduma <tevin74@live.com>
2023-06-14 13:15:14 +0300

    include backend app for portfolio details

---
+++ backend/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.http import *
+
+def projectDetails():
+    projects = {
+        "Poza": {
+            "Desc": "A Django-powered health clinic that facilitates online booking and medical chatbots",
+            "Github": "https://github.com/kurosakiaduma/poza",
+            "Deployment": "https://railway.up.poza.app",
+            },
+        "Anime Industry KPIs":{
+            "Desc": "A meticulous assessment of performance factors and indicators in the success of studio releases by conducting a meticulous study of data scraped from MyAnimeList.",
+            "Github": "https://github.com/kurosakiaduma/mal_analysis/",
+            "Deployment": "#",
+        },
+        "Sentiment Analysis of WeRateDogs’ Twitter":{
+            "Desc": "An in-depth study of WeRateDogs’ audience engagement model and shifts based on preference of dog breeds.",
+            "Github": "https://github.com/kurosakiaduma/mal_analysis/",
+            "Deployment": "#",
+        },
+    }
+    return projects

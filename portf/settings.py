c7cfaf9a69f3729340ce7a380e65e5002e1adf9c
Tevin Aduma <tevin74@live.com>
2023-06-15 22:48:45 +0300

    redo staticfiles paths

--- portf/settings.py
+++ portf/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'frontend' / 'static',
-]
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = "/static/"
+STATIC_ROOT = Path.joinpath(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR, "frontend", "static")]
+
+# Whitenoise Configuration
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

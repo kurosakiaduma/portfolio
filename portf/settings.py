f9b069c309b024b3c5e9b72748cd2ce8bd7bd745
Tevin Aduma <tevin74@live.com>
2023-06-15 22:05:42 +0300

    static files config

--- portf/settings.py
+++ portf/settings.py
@@ -119,8 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = "frontend/static"
-STATIC_URL = "static/"
+STATIC_ROOT = "../static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend' / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
